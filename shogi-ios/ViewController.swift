09e2dd1cda2d40cd194dd7e7b70ea67c757348e9
kagehito-rgb <npu3tdz3mt@i.softbank.jp>
2020-03-15 14:25:04 +0900

    gitignoreなど初期ファイル追加

---
+++ shogi-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shogi-ios
+//
+//  Created by 中村裕紀 on 2020/03/15.
+//  Copyright © 2020 kagehito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ shogi-iosTests/shogi_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  shogi_iosTests.swift
+//  shogi-iosTests
+//
+//  Created by 中村裕紀 on 2020/03/15.
+//  Copyright © 2020 kagehito. All rights reserved.
+//
+
+import XCTest
+@testable import shogi_ios
+
+class shogi_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
